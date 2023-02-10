77376615cc7534c05605bfc1881596ac40ae8c4e
PutraM <122906574+zanozaan@users.noreply.github.com>
2023-02-10 16:36:19 +0700

    Update Kalkulator Sederhana.py

--- Kalkulator Sederhana.py
+++ Kalkulator Sederhana.py
@@ -1,32 +1,22 @@
 print("Kalkulator Sederhana")
 print("="*20)
 
-def penjumlahan(x,y):
-    return x+y
+angka1 = float(input("angka pertama: "))
+operasi = input("Masukkan operasi yang ingin anda proses : ")
+angka2 = float(input("angka kedua :"))
 
-def pengurangan(x,y):
-    return x-y
-
-def perkalian(x,y):
-    return x*y
-
-def pembagian(x,y):
-    return x/y
-
-tipe= input("Masukkan angka yang ingin anda proses : ")
-
-if tipe in ("1", "2", "3", "4", "5"):
-    angka1 = float(input("angka pertama: "))
-    angka2 = float(input("angka kedua :"))
-
-if tipe == "1":
-    print("Hasil: ", penjumlahan(angka1,angka2))
-
-if tipe == "2":
-    print("Hasil: ", pengurangan(angka1,angka2))
-
-if tipe == "3":
-    print("Hasil: ", perkalian(angka1,angka2))
-
-if tipe == "4":
-    print("Hasil: ", pembagian(angka1,angka2))
+if operasi == "+":
+    penjumlahan = angka1 + angka2
+    print("Hasil: ", penjumlahan)
+    
+elif operasi == "-":
+    pengurangan = angka1 - angka2
+    print("Hasil: ", pengurangan)
+    
+elif operasi == "*":
+    perkalian = angka1 * angka2
+    print("Hasil: ", perkalian)
+    
+elif operasi == "/":
+    pembagian = angka1 / angka2
+    print("Hasil: ", pembagian)
